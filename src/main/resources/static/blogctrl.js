ecdad7d4edc2ea7ccd0f5d3d2e5e5fd4fbc56495
maenginner <mauricio.giraldo@mail.escuelaing.edu.co>
2015-11-18 19:16:46 -0500

    TERMINADO

---
+++ src/main/resources/static/blogctrl.js
@@ -0,0 +1,58 @@
+var app = angular.module("app",[]);
+
+app.controller("blogCtrl", function($scope,$log,$http) {
+    $scope.entry = {title : "Title",
+                    content : "Content"};
+    $scope.entries = [];
+    $log.debug('se creo el $scope');
+
+    $scope.loadData = function() {
+        var configList = {
+                method: "GET",
+                url: "blogs"
+                    };
+
+        var response=$http(configList);
+
+        response.success(function(data, status, headers, config) {
+            $scope.entries = data;
+            });
+
+        response.error(function(data, status, headers, config) {
+            alert("Ha fallado la petición. Estado HTTP:"+status);
+            });
+    };
+    
+      $scope.processForm = function() {
+        $log.debug($scope.entry);
+        $http({
+            method  : 'POST',
+            url     : 'blog',
+            data    : $scope.entry
+        }).success(function(data) {
+            console.log(data);
+            $scope.loadData();
+        });
+    };
+    
+    
+    $scope.delete = function(idx){
+        $log.debug ($scope.entry);
+        $http({
+            method: 'POST',
+            url:  'del',
+            data: idx  
+        }).success(function(data){
+           console.log(data);
+           $scope.loadData(); 
+        });       
+    };
+    
+    $scope.edit = function(idx){
+        $scope.delete(idx);
+        $scope.processForm();  
+    };
+    
+    
+    $scope.loadData();
+});
